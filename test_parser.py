44bd966df1e85ac17efdb3a13795bb4252eb8f3f
Tobias Hunger <tobias.hunger@gmail.com>
2018-01-22 09:17:29 +0100

    Split tests

--- test_parser.py
+++ test_parser.py
@@ -17,41 +17,68 @@ class ParserTest(unittest.TestCase):
         parser.Parser._commands[self._cmd1] = (self._cmd1, '<built-in>/1')
         parser.Parser._commands[self._cmd2] = (self._cmd2, '<built-in>/2')
 
-    def parse(self, data):
+
+    def _parse(self, data):
         return tuple(map(lambda e : (e._name, e._args), self.parser._parse_lines(data)))
 
-    def test_comments_and_empty_lines(self):
-        data = ('# test comment 1\n',
-                '   \t    # test comment 2     \n',
-                '####### test comment 3 ######\n',
-                ' \t \t    \n',
-                '\n',
-                '\t   ',
-                '')
+    def _verify(self, data, expected):
+        if data is str:
+            data = tuple(data)
+        result = self._parse(data)
+        self.assertEqual(result, expected)
 
-        result = self.parse(data)
-        self.assertEqual(result, ())
 
-    def test_commands(self):
-        data = ('    test1 foo bar\n',
-                ' \t test2 bar\n',
-                '  test2#comment\n',
-                '     test1\n',
-                'test1# comment 2\n',
-                '   test1   \t # comment 3\n',
-                'test1 baz 12345\n',
-                'test2')
+    def test_comment(self):
+        self._verify('# test comment\n', ())
 
-        result = self.parse(data)
-        self.assertEqual(result,
-                         ((self._cmd1, ' foo bar\n'),
-                          (self._cmd2, ' bar\n'),
-                          (self._cmd2, None),
-                          (self._cmd1, '\n'),
-                          (self._cmd1, None),
-                          (self._cmd1, '   \t # comment 3\n'),
-                          (self._cmd1, ' baz 12345\n'),
-                          (self._cmd2, None)))
+    def test_comment_with_leading_ws(self):
+        self._verify(' \t  # test comment\n', ())
+
+    def test_comment_without_nl(self):
+        self._verify(' # test comment', ())
+
+    def test_ws_only(self):
+        self._verify(' \t \t \n', ())
+
+    def test_nl_only(self):
+        self._verify('\n', ())
+
+    def test_empty_line(self):
+        self._verify('', ())
+
+    def test_command(self):
+        self._verify('test1\n', (self._cmd1, None))
+
+    def test_command_no_nl(self):
+        self._verify('test1', (self._cmd1, None))
+
+    def test_command_leading_ws(self):
+        self._verify(' \t test1\n', (self._cmd1, None))
+
+    def test_command_leading_ws_no_nl(self):
+        self._verify(' \ttest1', (self._cmd1, None))
+
+    def test_command_comment(self):
+        self._verify('test1# comment\n', (self._cmd1, None))
+
+    def test_command_ws_comment(self):
+        self._verify('test1   # comment\n', (self._cmd1, None))
+
+    def test_command_one_arg(self):
+        self._verify(' test1  arg1 \n', (self._cmd1, ('arg1  \n'))) # FIXME
+
+    def test_command_two_args(self):
+        self._verify('test1  arg1  arg2\n', (self._cmd1, ('arg1   arg3\n'))) # FIXME
+
+    def test_command_one_arg_no_nl(self):
+        self._verify('test1  arg1', (self._cmd1, ('arg1')))
+
+    def test_command_two_args_no_nl(self):
+        self._verify('test1   arg1  arg2', (self._cmd1, ('arg1  arg2'))) # FIXME
+
+    # FIXME Add string and multiline string tests
+
+    # Error cases:
 
     def test_assert_on_unknown_command(self):
        with self.assertRaises(exceptions.ParseError):
