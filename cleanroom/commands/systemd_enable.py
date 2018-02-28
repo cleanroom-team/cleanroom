d0e9463934cd397ebc6bf7bf73161086864d114a
Tobias Hunger <tobias.hunger@gmail.com>
2018-02-28 08:19:26 +0100

    Implement systemd_enable command

--- cleanroom/commands/systemd_enable.py
+++ cleanroom/commands/systemd_enable.py
@@ -6,10 +6,6 @@
 
 import cleanroom.command as cmd
 import cleanroom.exceptions as ex
-import cleanroom.helper.generic.file as file
-import cleanroom.parser as parser
-
-import os.path
 
 
 class SystemdEnableCommand(cmd.Command):
@@ -23,11 +19,14 @@ class SystemdEnableCommand(cmd.Command):
     def validate_arguments(self, file_name, line_number, *args, **kwargs):
         """Validate the arguments."""
         if len(args) == 0:
-            raise ex.ParseError('systemd_enable needs at least one unit to enable.',
+            raise ex.ParseError('systemd_enable needs at least one unit '
+                                'to enable.',
                                 file_name=file_name, line_number=line_number)
 
         return None
 
     def __call__(self, file_name, line_number, run_context, *args, **kwargs):
         """Execute command."""
-        pass
+        run_context.run('/usr/bin/systemctl',
+                        '--root={}'.format(run_context.fs_directory()),
+                        'enable', *args, outside=True)
