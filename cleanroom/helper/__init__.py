72dca8afe9f6ce23f0739a58020ef283c5c62f4d
Tobias Hunger <tobias.hunger@gmail.com>
2018-04-12 22:07:47 +0200

    More __init__.py files

---
+++ cleanroom/helper/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""cleanroom.helper Module.
+
+@author: Tobias Hunger <tobias.hunger@gmail.com>
+"""
+
+pass
---
+++ cleanroom/helper/generic/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""cleanroom.helper.generic Module.
+
+@author: Tobias Hunger <tobias.hunger@gmail.com>
+"""
+
+pass
