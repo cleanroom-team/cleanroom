c13a897d056f52d06d7d9d8a2001c4667c51bf7a
Tobias Hunger <tobias.hunger@gmail.com>
2018-09-06 22:34:16 +0200

    Fix register_container command
    Actually register all necessary information.

--- cleanroom/generator/commands/register_container.py
+++ cleanroom/generator/commands/register_container.py
@@ -18,7 +18,8 @@ class RegisterContainerCommand(Command):
         super().__init__('register_container', syntax='<SYSTEM> '
                          'description=<DESC> extra_args=<ARG>(,<ARG>)* '
                          'timeout=3m after=<SYSTEM>(,<SYSTEM>)* '
-                         'requires=<SYSTEM>(,<SYSTEM>)*',
+                         'requires=<SYSTEM>(,<SYSTEM>)*'
+                         'enable=False',
                          help='Register a container with a system.',
                          file=__file__)
 
@@ -27,16 +28,15 @@ class RegisterContainerCommand(Command):
         self._validate_args_exact(location, 1, '"{}" needs a system to '
                                   'install as a container.', *args)
         self._validate_kwargs(location, ('description', 'extra_args', 'after',
-                                         'requires', 'timeout'),
+                                         'requires', 'timeout', 'enable'),
                               **kwargs)
         self._require_kwargs(location, ('description',), **kwargs)
 
-    def _nspawnify(*systems):
-        result = ' '.join(map(lambda a: 'systemd-nspawn@{}.service'.format(a),
-                              systems))
-        if result:
-            result += '\n'
-        return result
+    def _nspawnify(self, what, *systems):
+        clean = [s for s in systems if s]
+        if len(clean) > 0:
+            return '\n{}={}'.format(what, ' '.join(*clean))
+        return ''
 
     def __call__(self, location, system_context, *args, **kwargs):
         """Execute command."""
@@ -46,6 +46,7 @@ class RegisterContainerCommand(Command):
         after_input = kwargs.get('after', '')
         requires_input = kwargs.get('requires', '')
         timeout = kwargs.get('timeout', '3m')
+        enable = kwargs.get('enable', False)
 
         bin_directory = '/usr/bin'
         systemd_directory = '/usr/lib/systemd/system'
@@ -61,32 +62,32 @@ class RegisterContainerCommand(Command):
                                '/usr/bin/update-container.sh "{}" || exit 1\n')
 
         location.set_description('')
-        system_context.execute(location.next_line(), 'mkdir', '{}/systemd-nspawn@{}.d'
-                               .format(systemd_directory, system))
+        override_dir = '{}/systemd-nspawn@{}.service.d'.format(systemd_directory, system)
+        system_context.execute(location.next_line(), 'mkdir', override_dir)
 
-        extra_args = '\n'.join(extra_args_input.split(','))
+        extra_args = ' \\\n    '.join(extra_args_input.split(','))
         if extra_args:
-            extra_args = '\n' + extra_args + '\n'
-        after = self._nspawnify(after_input.split(','))
-        requires = self._nspawnify(requires_input.split(','))
+            extra_args = ' \\\n    ' + extra_args + '\n'
+        after = self._nspawnify('After', *after_input.split(','))
+        requires = self._nspawnify('Requires', *requires_input.split(','))
 
         system_context.execute(location.next_line(), 'create',
-                               '{}/systemd-nspawn@{}.d/'
-                               'override.conf'
-                               .format(systemd_directory, system), '''[Unit]
-Description=Container {system}: {description}
-{after}{requires}
+                               '{}/override.conf'.format(override_dir),
+                               '''[Unit]
+Description=Container {system}: {description}{after}{requires}
+
 [Service]
-TimeoutStartSec={timeout}m
+TimeoutStartSec={timeout}
 ExecStart=
-ExecStart=/usr/bin/systemd-nspawn --quiet --keep-unit --boot --ephemeral \
+ExecStart=/usr/bin/systemd-nspawn --quiet --keep-unit --boot --ephemeral \\
     --machine={system}{extra_args}
 '''.format(system=system, description=description, after=after,
            requires=requires, extra_args=extra_args, timeout=timeout))
 
-        location.set_description('Enabling container')
-        system_context.execute(location.next_line(), 'symlink',
-                               '../systemd-nspawn@.service',
-                               'systemd-nspawn@{}.service'.format(system),
-                               base_directory='{}/machines.target.wants'
-                               .format(systemd_directory))
+        if enable:
+            location.set_description('Enabling container')
+            system_context.execute(location.next_line(), 'symlink',
+                                   '../systemd-nspawn@.service',
+                                   'systemd-nspawn@{}.service'.format(system),
+                                   base_directory='{}/machines.target.wants'
+                                   .format(systemd_directory))
