69bd5a5e803de434e250b29348449d2eae85aac2
Tobias Hunger <tobias.hunger@gmail.com>
2018-12-16 20:47:06 +0100

    Support all supported arch kernels mentioned in the wiki

--- cleanroom/generator/commands/normalize_kernel_install.py
+++ cleanroom/generator/commands/normalize_kernel_install.py
@@ -26,21 +26,16 @@ class NormalizeKernelInstallCommand(Command):
     def __call__(self, location, system_context, *args, **kwargs):
         """Execute command."""
         location.set_description('Handle different kernel flavors')
-        system_context.execute(location, 'move',
-                               '/etc/mkinitcpio.d/linux-hardened.preset',
-                               '/etc/mkinitcpio.d/linux.preset',
-                               ignore_missing_sources=True)
-        system_context.execute(location.next_line(), 'move',
-                               '/etc/mkinitcpio.d/linux-lts.preset',
-                               '/etc/mkinitcpio.d/linux.preset',
-                               ignore_missing_sources=True)
-        system_context.execute(location.next_line(), 'move',
-                               '/etc/mkinitcpio.d/linux-git.preset',
-                               '/etc/mkinitcpio.d/linux.preset',
-                               ignore_missing_sources=True)
+        for kernel in ("linux-hardened", "linux-lts", "linux-zen", "linux-git"):
+            system_context.execute(location, 'move',
+                                   '/etc/mkinitcpio.d/{}.preset'.format(kernel),
+                                   '/etc/mkinitcpio.d/linux.preset',
+                                   ignore_missing_sources=True)
 
-        system_context.execute(location.next_line(), 'move','/boot/vmlinuz-linux*',
-                               '/boot/vmlinuz')
+            system_context.execute(location.next_line(), 'move',
+                                   '/boot/vmlinuz-{}'.format(kernel),
+                                   '/boot/vmlinuz')
+
         system_context.execute(location.next_line(), 'copy','/boot/vmlinuz',
                                os.path.join(system_context.boot_data_directory(), 'vmlinuz'),
                                to_outside=True)
