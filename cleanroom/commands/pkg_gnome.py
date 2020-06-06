fd10637ed35143e14a2e81ce31da98cd0f654f05
Tobias Hunger <tobias.hunger@gmail.com>
2020-06-06 16:09:01 +0200

    pkg_desktop: Move code into pkg_gnome and pkg_desktop a symlink
    Move the code from pkg_desktop into pkg_gnome and make pkg_desktop a
    symlink to that. This should make it easier to provide different
    desktop options down the road.
    
    Slim down the pkg_gnome install a bit by trimming out unnecessary
    dependencies.

---
+++ cleanroom/commands/pkg_gnome.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""pkg_gnome command.
+
+@author: Tobias Hunger <tobias.hunger@gmail.com>
+"""
+
+
+from cleanroom.command import Command
+from cleanroom.helper.file import remove
+from cleanroom.location import Location
+from cleanroom.systemcontext import SystemContext
+
+import typing
+
+
+class PkgGnomeCommand(Command):
+    """The pkg_gnome command."""
+
+    def __init__(self, **services: typing.Any) -> None:
+        """Constructor."""
+        super().__init__(
+            "pkg_gnome",
+            help_string="Install the Gnome desktop environment",
+            file=__file__,
+            **services
+        )
+
+    def validate(
+        self, location: Location, *args: typing.Any, **kwargs: typing.Any
+    ) -> None:
+        """Validate the arguments."""
+        self._validate_no_arguments(location, *args, **kwargs)
+
+        return None
+
+    def __call__(
+        self,
+        location: Location,
+        system_context: SystemContext,
+        *args: typing.Any,
+        **kwargs: typing.Any
+    ) -> None:
+        """Execute command."""
+        self._execute(
+            location,
+            system_context,
+            "pacman",
+            "--assume-installed",
+            "jack",
+            "--assume-installed",
+            "qt5-base",
+            "--assume-installed",
+            "qt5-declarative",
+            "--assume-installed",
+            "qt5-wayland",
+            "--assume-installed",
+            "qt5-x11extras",
+            "baobab",
+            "brasero",
+            "cheese",
+            "chrome-gnome-shell",
+            "file-roller",
+            "p7zip",
+            "unrar",
+            "unace",
+            "lrzip",
+            "gdm",
+            "gnome-color-manager",
+            "gnome-control-center",
+            "gnome-keyring",
+            "gnome-menus",
+            "gnome-screenshot",
+            "gnome-shell",
+            "gnome-shell-extensions",
+            "gnome-system-monitor",
+            "gnome-terminal",
+            "gnome-tweaks",
+            "gvfs",
+            "gvfs-afc",
+            "gvfs-goa",
+            "gvfs-google",
+            "gvfs-gphoto2",
+            "gvfs-mtp",
+            "gvfs-smb",
+            "mousetweaks",
+            "nautilus",
+            "networkmanager",
+            "networkmanager-openvpn",
+            "networkmanager-vpnc",
+            "pavucontrol",
+            "sane",
+            "sound-juicer",
+            "tracker-miners",
+            "usb_modeswitch",
+            "xdg-user-dirs-gtk",
+            "xorg-server-xwayland",
+            "gnome-remote-desktop",
+            "gnome-user-share",
+            "rygel",
+            "system-config-printer",
+        )
+
+        # Remove useless GStreamer plugins:
+        remove(
+            system_context,
+            "/usr/lib/gstreamer-*/libgstjack.so",
+            "/usr/lib/gstreamer-*/libgstqmlgl.so",
+            force=True,
+        )
+
+        location.set_description("networkmanager fixup")
+        self._execute(
+            location.next_line(),
+            system_context,
+            "create",
+            "/usr/lib/tmpfiles.d/networkmanager.conf",
+            """d /var/etc/NetworkManager 0700 root root
+d /var/etc/NetworkManager/system-connections 0750 root root
+""",
+            mode=0o644,
+        )
+
+        self._execute(
+            location.next_line(),
+            system_context,
+            "systemd_enable",
+            "NetworkManager.service",
+            "NetworkManager-dispatcher.service",
+        )
+        #        self._execute(location.next_line(), system_context, 'remove',
+        #                      '/usr/lib/systemd/system/'
+        #                      'dbus-org.freedesktop.nm-dispatcher.service',
+        #                      '/usr/lib/systemd/system/'
+        #                      dbus-org.freedesktop.NetworkManager.service')
+        self._execute(
+            location.next_line(),
+            system_context,
+            "symlink",
+            "NetworkManager.service",
+            "dbus-org.freedesktop.NetworkManager.service",
+            work_directory="/usr/lib/systemd/system",
+        )
+        self._execute(
+            location.next_line(),
+            system_context,
+            "symlink",
+            "NetworkManager-dispatcher.service",
+            "dbus-org.freedesktop.nm-dispatcher.service",
+            work_directory="/usr/lib/systemd/system",
+        )
+
+        self._execute(
+            location.next_line(),
+            system_context,
+            "mkdir",
+            "/var/etc/NetworkManager/system-connections",
+        )
+        self._execute(
+            location.next_line(),
+            system_context,
+            "remove",
+            "/etc/NetworkManager/system-connections",
+            recursive=True,
+            force=True,
+        )
+        self._execute(
+            location.next_line(),
+            system_context,
+            "symlink",
+            "../../var/etc/NetworkManager/system-connections",
+            "system-connections",
+            work_directory="/etc/NetworkManager",
+        )
+        self._add_hook(
+            location.next_line(),
+            system_context,
+            "export",
+            "remove",
+            "/usr/share/gtk-doc/html/*",
+            recursive=True,
+            force=True,
+        )
+
+        self._execute(
+            location.next_line(), system_context, "systemd_enable", "gdm.service"
+        )
